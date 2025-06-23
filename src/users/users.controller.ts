f23f2a9ae917869c5239e6f09c21b48cfd44c052
Attila Horvath <sweeptic@gmail.com>
2025-06-23 13:42:00 +0200

    19. Globally Scoped Interceptors

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,7 +9,6 @@ import {
     Post,
     Query,
     Session,
-    UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -18,12 +17,10 @@ import { Serialize } from 'src/interceptors/serialize.interceptors';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './user.entity';
 
 @Serialize(UserDto)
 @Controller('auth')
-@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
