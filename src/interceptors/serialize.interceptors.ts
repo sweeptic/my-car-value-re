cd6ecc7081dacb5bfd8526bd94168ceb00ad182e
Attila Horvath <sweeptic@gmail.com>
2025-06-23 13:14:15 +0200

    15. Why a Decorator and Interceptor

--- src/interceptors/serialize.interceptors.ts
+++ src/interceptors/serialize.interceptors.ts
@@ -23,7 +23,7 @@ export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // Run something before a request is handled
 
-    console.log("I'm running before the handler");
+    // console.log("I'm running before the handler");
 
     return handler.handle().pipe(
       map((data: any) => {
--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -1,7 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
-  (data: any, context: ExecutionContext) => {
+  (data: never, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    console.log(request.session.userId);
+
     return 'hi there';
   },
 );
