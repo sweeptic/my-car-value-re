87c1bdd55776ae48a2871afff0fe60fa47a1c211
Attila Horvath <sweeptic@gmail.com>
2025-06-23 12:01:27 +0200

    07. Handling User Sign In

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -14,6 +18,7 @@ export class AuthService {
 
     if (users.length) {
       throw new BadRequestException('email in use');
+      //   throw new NotFoundException('User not found');
     }
 
     // Hash the users password
@@ -33,5 +38,26 @@ export class AuthService {
     // Return the user
     return user;
   }
-  signin() {}
+  //
+  //
+  async signin(email: string, password: string) {
+    //12 test01@test.com    7eb998072756a62a.c6b0778a3ea4dda697486b03666a4836bef12d304dc31f07c9a0573a3060463d
+    //13 test02@test.com    afce969fa8a8c455.1e4e9f5afd50482e165783d9c98cbe649dcf83f378c18b04259c750dd59f38b6
+
+    const [user] = await this.usersService.find(email);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('bad password');
+    }
+
+    return user;
+  }
 }
