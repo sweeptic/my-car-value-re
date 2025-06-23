13e2e0250e544ed03b0f662e03aa4c36b4a18f0b
Attila Horvath <sweeptic@gmail.com>
2025-06-23 13:52:46 +0200

    05. Applying a Globally Scoped Middleware

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(
-    cookieSession({
-      keys: ['some-nasty-keys-here'],
-    }),
-  );
-
-  //   app.useGlobalPipes(
-  //     new ValidationPipe({
-  //       whitelist: true,
-  //     }),
-  //   );
   await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
