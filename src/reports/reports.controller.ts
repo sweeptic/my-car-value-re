1f15de62d59cc3df3c1e6ad02009a2c712f932a1
Attila Horvath <sweeptic@gmail.com>
2025-06-24 19:40:10 +0200

    01. Creating a Query Builder

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -39,5 +39,6 @@ export class ReportsController {
   @Get()
   getEstimate(@Query() query: GetEstimateDto) {
     console.log('getEstimate', query);
+    return this.reportsService.createEstimate(query);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { Report } from './report.entity';
 import { User } from 'src/users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
@@ -29,5 +30,13 @@ export class ReportsService {
 
     report.approved = approved;
     return this.repo.save(report);
+  }
+
+  createEstimate(estimateDto: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('*')
+      .where('make = :make', { make: estimateDto.make })
+      .getRawMany();
   }
 }
