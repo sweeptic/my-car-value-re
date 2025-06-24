429457eebd2afafe0be367cf2965b9d874445a2e
Attila Horvath <sweeptic@gmail.com>
2025-06-24 19:22:56 +0200

    08. Assigning CurrentUser with a Middleware

--- src/users/interceptors/current-user.interceptor.ts
+++ src/users/interceptors/current-user.interceptor.ts
@@ -1,25 +1,25 @@
-import { UsersService } from '../users.service';
-import {
-    CallHandler,
-    ExecutionContext,
-    Injectable,
-    NestInterceptor,
-} from '@nestjs/common';
+// import { UsersService } from '../users.service';
+// import {
+//     CallHandler,
+//     ExecutionContext,
+//     Injectable,
+//     NestInterceptor,
+// } from '@nestjs/common';
 
-@Injectable()
-export class CurrentUserInterceptor implements NestInterceptor {
-  constructor(private UsersService: UsersService) {}
+// @Injectable()
+// export class CurrentUserInterceptor implements NestInterceptor {
+//   constructor(private UsersService: UsersService) {}
 
-  async intercept(context: ExecutionContext, handler: CallHandler) {
-    console.log('CurrentUserInterceptor');
-    const request = context.switchToHttp().getRequest();
-    const { userId } = request.session;
+//   async intercept(context: ExecutionContext, handler: CallHandler) {
+//     console.log('CurrentUserInterceptor');
+//     const request = context.switchToHttp().getRequest();
+//     const { userId } = request.session;
 
-    if (userId) {
-      const user = await this.UsersService.findOne(userId);
-      request.currentUser = user;
-    }
+//     if (userId) {
+//       const user = await this.UsersService.findOne(userId);
+//       request.currentUser = user;
+//     }
 
-    return handler.handle();
-  }
-}
+//     return handler.handle();
+//   }
+// }
