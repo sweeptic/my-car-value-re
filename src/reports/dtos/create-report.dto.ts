48b07dacd589a93e1112afba98ff0fc44ecd5f0b
Attila Horvath <sweeptic@gmail.com>
2025-06-24 15:18:00 +0200

    03. A DTO for Report Creation

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,17 @@
+export class CreateReportDto {
+  id: number;
+
+  price: string;
+
+  make: string;
+
+  model: string;
+
+  year: string;
+
+  lng: string;
+
+  lat: string;
+
+  mileage: string;
+}
