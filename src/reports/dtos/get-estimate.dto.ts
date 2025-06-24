4e2a2cb848bdc4543bf18445101096dfee720b92
Attila Horvath <sweeptic@gmail.com>
2025-06-24 19:33:40 +0200

    10. Validating Query String Values

---
+++ src/reports/dtos/get-estimate.dto.ts
@@ -0,0 +1,35 @@
+import {
+    IsLatitude,
+    IsLongitude,
+    IsNumber,
+    IsString,
+    Max,
+    Min,
+} from 'class-validator';
+
+export class GetEstimateDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+
+  @IsNumber()
+  mileage: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+}
